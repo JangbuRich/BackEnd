d1fa94a4867fe7dbc65e704979abb7fb50390321
hkjin <wlsgusrb78@naver.com>
2025-09-02 23:12:06 +0900

    :truck: 패키지 위치 변경에 따른 코드 수정

--- src/main/java/com/jangburich/config/AmazonS3Config.java
+++ src/main/java/com/jangburich/config/AmazonS3Config.java
@@ -1,4 +1,4 @@
-package com.jangburich.config.s3;
+package com.jangburich.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/jangburich/global/common/S3Service.java
+++ src/main/java/com/jangburich/global/common/S3Service.java
@@ -1,4 +1,4 @@
-package com.jangburich.config.s3;
+package com.jangburich.global.common;
 
 import java.io.IOException;
 import java.util.UUID;
